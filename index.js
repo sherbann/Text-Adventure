1919a724a79a56aa388c73db910f1fba09566439
sherbann <93603838+sherbann@users.noreply.github.com>
2021-11-30 00:41:11 +0000

    stage complete

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 /**this function is hiding first parameter and showing the second **/
 function showElement(show, hide) {
-    console.log(score, show, hide)
+    let nextScore = show[show.length - 1];
     document.getElementById(hide).style.display = "none";
-    document.getElementById(show).style.display = "inline-block";
+    document.getElementById(show).style.display = "inline-block";   
+    
+    document.getElementById("score" + nextScore).innerHTML =
+        "Total score: " + score + " out of 600 points";
 }
 
 let score = 0;
@@ -10,28 +13,27 @@ let score = 0;
 * disables the submit buttons */
 function submitCharacter(AnswerX, btnX, scoreX) {
 
-         validateForm(AnswerX);
+    validateForm(AnswerX);
     score = pointsValidation(AnswerX);
-    let msg = "Total score: "  + score +   " out of 900 points";
+    let msg = "Total score: " + score + " out of 600 points";
     document.getElementById(scoreX).innerHTML = msg;
     /*const element=document.getElementsByClassName("ScoreX");//.innerHTML = msg;
     element.innerHTML= msg;*/
-    document.getElementById(btnX).disabled="true"
-    document.getElementById(AnswerX).value=""
+    document.getElementById(btnX).disabled = "true"
+    document.getElementById(AnswerX).value = ""
 }
 
 function submitQuestion(AnswerX, btnX, scoreX) {
     validateForm(AnswerX);
     score = pointsValidation(AnswerX);
-    let msg = "Total score: " + score + " out of 900 points";
+    let msg = "Total score: " + score + " out of 600 points";
     document.getElementById(scoreX).innerHTML = msg;
-    //document.getElementsByClassName("ScoreX").innerHTML = msg;
     document.getElementById(btnX).disabled = "true"
     document.getElementById(AnswerX).value = ""
 
-
-
 }
+
+
 /** function updates score when a picture is clicked */
 function clickedPicture(pic, scoreP) {
     let pictures = ["img1", "img3", "img6", "img8", "img10"]
@@ -41,14 +43,13 @@ function clickedPicture(pic, scoreP) {
     } else {
         score = score - y;
     }
-    let msg = "Total score: " + score + " out of 900 points";
+    let msg = "Total score: " + score + " out of 600 points";
     document.getElementById(scoreP).innerHTML = msg;
-    document.getElementById("img1").disabled = "true"
-    document.getElementById("img2").disabled = "true"
-
-
+    document.getElementById(pic).disabled = "true"
+    
 
 }
+
 /** function to validate user input */
 function validateForm(AnswerX) {
     let answer = document.getElementById(AnswerX).value;
@@ -60,10 +61,11 @@ function validateForm(AnswerX) {
 /** function updates score based on user answers */
 function pointsValidation(AnswerX) {
     const submitChar = ["padlock", "padlock key", "chains", "jug of water", "golden dagger",
-        "sharp sword", "kingdom map", "encrusted shield", "seal", "crown", "sceptre", "jewelry",
-        "golden crosses", "ancient scroll", "prayer book", "candle", "helmet", "axe", "shield",
+        "sharp sword", "kingdom map", "encrusted shield", "royal seal", "crown", "sceptre", "jewelry",
+        "golden cross", "ancient scroll", "the bible", "candle", "helmet", "axe", "shield",
         "hammer", "spears"];
-    const submitEnemy = ["Sam", "sam", "oasis", "Oasis", "anchor", "Anchor", "Stone Keep", "Stone keep", "stone keep", "Windsor Castle"];
+    const submitEnemy = ["Sam", "sam", "oasis", "Oasis", "anchor", "Anchor", "Keep", "keep", 
+    "stone keep", "Windsor", "windsor"];
     let updateScore = 0;
     let x = getRandomIntInclusive(50, 80)
     let answer = document.getElementById(AnswerX).value
@@ -74,13 +76,14 @@ function pointsValidation(AnswerX) {
     } else if (!submitChar.includes(answer)) {
         updateScore = score;
     } else if (!submitEnemy.includes(answer)) {
-        updateScore = score;
+        updateScore = score - x;
     }
     return updateScore;
 }
 /** function updates score based on user answers */
 function questionPoints(AnswerX) {
-    const submitEnemy = ["Sam", "sam", "oasis", "Oasis", "anchor", "Anchor", "Stone Keep", "Stone keep", "stone keep"];
+    const submitEnemy = ["Sam", "sam", "oasis", "Oasis", "anchor", "Anchor", "Stone Keep",
+     "Stone keep", "stone keep"];
     let updateScore = 0;
     let x = getRandomIntInclusive(50, 80)
     let answer = document.getElementById(AnswerX).value
@@ -94,6 +97,7 @@ function getRandomIntInclusive(min, max) {
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
+
 
 
 
